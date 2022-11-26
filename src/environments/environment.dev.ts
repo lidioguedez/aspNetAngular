984f704fdcd84a4e4734c4c525d31b08fbe0420b
Lidio Guedez <112365426lidioguedez@users.noreply.github.com>
2022-11-26 14:23:20 -0300

    add environments

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  name: "dev",
+  firebase: {
+    config : {
+    }
+  }
+};
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  name: "prod",
+  firebase: {
+    config : {
+    }
+  }
+};
