6e28e75a906ae66a5981f8dfe1b7eeeaefbef10b
lidioguedez <112365426+lidioguedez@users.noreply.github.com>
2022-11-28 21:20:54 -0300

    creando el primer componente de ejemplo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {AngularFireModule} from "@angular/fire/compat";
-import {AngularFirestoreModule} from "@angular/fire/compat/firestore";
-import {AngularFireAuthModule} from "@angular/fire/compat/auth";
-import {AngularFireStorageModule} from "@angular/fire/compat/storage";
-import {environment} from "@src/environments/environment.dev";
 import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
@@ -17,11 +12,7 @@ import { HeaderComponent } from './components/header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebase.config),
-    AngularFirestoreModule,
-    AngularFireAuthModule,
-    AngularFireStorageModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
